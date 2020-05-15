368576e79ef43954abbcda83a65c7b0315ef51f1
chriskorsak <chris@chriskorsak.com>
2020-05-14 21:06:42 -0700

    add business logic to word with 2 consonants at the begining.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 //Business Logic
 const pigLatin = function(translation) {
   if (translation === "a" || translation === "e" || translation === "i" || translation === "o" || translation === "u")  {
-    return translation + "way";
-} else {
-    return translation.slice(1) + translation.charAt(0) + "ay";
+      return translation + "way";
+    } else if (translation.charAt(0) != "a" && translation.charAt(0) != "e" && translation.charAt(0) != "i" && translation.charAt(0) != "o" && translation.charAt(0) != "u" && translation.charAt(1) != "a")  {
+      return translation.slice(2) + translation.charAt(0) + translation.charAt(1) + "ay";
+    } else {
+      return translation.slice(1) + translation.charAt(0) + "ay";
     // return translation + "ay";
 }
 
